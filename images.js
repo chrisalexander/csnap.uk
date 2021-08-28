fd6a2caa35dd37fc71e6b26df1c55a16f965e8b6
chrisalexander <chris@chris-alexander.co.uk>
2021-08-28 07:44:40 +0100

    Check to see if an image is immediately loaded (cache) to avoid jumping

--- images.js
+++ images.js
@@ -1,32 +1,37 @@
 var images = {
-    "fastTransition": 100,
-    "slowTransition": 300,
+    "fastTransition": 200,
+    "slowTransition": 450,
 
     "handleImgFade": function(el) {
         var matches = el.style.backgroundImage.match(/^url\("(.+)"\)$/);
         if (!matches || matches.length < 1) { return; }
+
         var url = matches[1]; 
-        el.className += " loading";
-        el.style.opacity = 0;
-        el.style.transition = "opacity " + this.fastTransition + "ms linear";
-        el.style.opacity = 1;
+        
+        var started = false;
+        var startTransitionTimeout = setTimeout(() => {
+            started = true;
+            el.className += " loading";
+            el.style.opacity = 0;
+            el.style.transition = "opacity " + this.fastTransition + "ms linear";
+            el.style.opacity = 1;
+        }, 1);
         
         var img = new Image();
-        var transitions = {
-            "fast": this.fastTransition,
-            "slow": this.slowTransition
-        }
-        img.onload = (function (el, url) {
+        img.onload = (function (el, url, fastTransition, slowTransition, startTransitionTimeout) {
             return function () {
+                clearTimeout(startTransitionTimeout);
+                if (!started) { return; }
+
                 el.style.opacity = 0;
                 setTimeout(() => {
                     el.style.backgroundImage = 'url("' + url + '")';
-                    el.style.transition = "opacity " + transitions.slow + "ms linear";
+                    el.style.transition = "opacity " + slowTransition + "ms linear";
                     el.className = el.className.replace(" loading", "");
                     el.style.opacity = 1;
-                }, transitions.fast * 1.1);
+                }, fastTransition * 1.1);
             }
-        })(el, url);
+        })(el, url, this.fastTransition, this.slowTransition, startTransitionTimeout);
         img.src = url;
     },
 
