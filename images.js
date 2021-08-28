dfc31b9bde5060c694fdc14a55d8752762e83df4
chrisalexander <chris@chris-alexander.co.uk>
2021-08-28 07:32:53 +0100

    Refactor

--- images.js
+++ images.js
@@ -1,33 +1,40 @@
 var images = {
+    "fastTransition": 100,
+    "slowTransition": 300,
+
+    "handleImgFade": function(el) {
+        var matches = el.style.backgroundImage.match(/^url\("(.+)"\)$/);
+        if (!matches || matches.length < 1) { return; }
+        var url = matches[1]; 
+        el.className += " loading";
+        el.style.opacity = 0;
+        el.style.transition = "opacity " + this.fastTransition + "ms linear";
+        el.style.opacity = 1;
+        
+        var img = new Image();
+        var transitions = {
+            "fast": this.fastTransition,
+            "slow": this.slowTransition
+        }
+        img.onload = (function (el, url) {
+            return function () {
+                el.style.opacity = 0;
+                setTimeout(() => {
+                    el.style.backgroundImage = 'url("' + url + '")';
+                    el.style.transition = "opacity " + transitions.slow + "ms linear";
+                    el.className = el.className.replace(" loading", "");
+                    el.style.opacity = 1;
+                }, transitions.fast * 1.1);
+            }
+        })(el, url);
+        img.src = url;
+    },
 
     "bgImgFade": function () {
-        var fastTransition = 100;
-        var slowTransition = 300;
         var els = document.getElementsByClassName("bgloadfade");
         for (var i = 0; i < els.length; i++) {
             var el = els[i];
-
-            var matches = el.style.backgroundImage.match(/^url\("(.+)"\)$/);
-            if (!matches || matches.length < 1) { continue; }
-            var url = matches[1]; 
-            el.className += " loading";
-            el.style.opacity = 0;
-            el.style.transition = "opacity " + fastTransition + "ms linear";
-            el.style.opacity = 1;
-            
-            var img = new Image();
-            img.onload = (function (el, url) {
-                return function () {
-                    el.style.opacity = 0;
-                    setTimeout(() => {
-                        el.style.backgroundImage = 'url("' + url + '")';
-                        el.style.transition = "opacity " + slowTransition + "ms linear";
-                        el.className = el.className.replace(" loading", "");
-                        el.style.opacity = 1;
-                    }, fastTransition * 1.1);
-                }
-            })(el, url);
-            img.src = url;
+            this.handleImgFade(el);
         }
     }
 }
